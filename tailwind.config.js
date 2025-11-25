8925a815d0ca88e046e69352fe69bd58ca446cc6
eXaive <royan.reddie@hotmail.com>
2025-11-24 23:17:30 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
